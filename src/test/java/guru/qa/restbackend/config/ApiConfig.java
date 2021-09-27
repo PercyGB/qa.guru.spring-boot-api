499758d58a53e3587fc77aa4e53d92ead41c388e
Liana Kuznetsova <48284407+PercyGB@users.noreply.github.com>
2021-09-27 23:27:23 +0400

    Update ApiConfig.java

--- src/test/java/guru/qa/restbackend/config/ApiConfig.java
+++ src/test/java/guru/qa/restbackend/config/ApiConfig.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:config/api/api.properties"
+        "classpath:config/api.properties"
 })
 public interface ApiConfig extends Config {
     @Key("baseUrl")
